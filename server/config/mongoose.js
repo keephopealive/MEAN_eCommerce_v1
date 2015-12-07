f6a2b2af201eee96c0c39c725564a00c0c340d83
Speros Misirlakis <smisirlakis@gmail.com>
2015-12-06 20:09:27 -0800

    mongoose changes

--- server/config/mongoose.js
+++ server/config/mongoose.js
@@ -3,10 +3,27 @@ var fs = require('fs');
 
 mongoose.connect('mongodb://localhost/MEAN_eCommerceSchema');
 
-var models_path = __dirname + '/../models';
+var mongoose = require('mongoose');
 
-fs.readdirSync(models_path).forEach(function(file) {
-	if (file.indexOf('.js') > 0) {
-		require(models_path + '/' + file);
-	}
-})
+var UserSchema = new mongoose.Schema({
+  first_name: { type: String, trim: true },
+  created_at: { type: Date, default: Date.now },
+  updated_at: { type: Date, default: Date.now }
+});
+
+mongoose.model('User', UserSchema);
+UserSchema.path('first_name').required(true, "First Name is required");
+
+
+
+
+
+
+
+// var models_path = __dirname + '/../models';
+
+// fs.readdirSync(models_path).forEach(function(file) {
+// 	if (file.indexOf('.js') > 0) {
+// 		require(models_path + '/' + file);
+// 	}
+// })
